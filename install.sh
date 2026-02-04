b4f8db2a6c9f56cd895497a2771d563c0b2b262a
Hantong Chen <cxwdyx620@gmail.com>
2026-02-04 23:53:23 +0800

    feat: v0

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+podman build \
+    -f ./Dockerfile.prod \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --build-arg http_proxy="${http_proxy:-""}" \
+    --build-arg https_proxy="${https_proxy:-""}" \
+    --format docker \
+    -t ghcr.io/han-rs/container-ci-freenginx:prod
+
+# Install container service
+/bin/cp -f ./assets/template.container ~/.config/containers/systemd/freenginx.container
+
+# Enable container service
+systemctl --user daemon-reload
+systemctl --user start freenginx
